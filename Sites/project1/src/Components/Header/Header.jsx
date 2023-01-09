bd4a7fdfecd42e9d058bec7e1c5b651bacb05b56
SlankehWeb <101549379+SlankehWeb@users.noreply.github.com>
2023-01-09 13:18:55 +0100

    hj

---
+++ Sites/project1/src/Components/Header/Header.jsx
@@ -0,0 +1,17 @@
+const Header = () => {
+    return (
+        <header>
+            <div class="banner">
+
+            </div>
+            <nav>
+               <a class="#" href="#">verdensmålene</a>
+               <a href="#">delmålene</a>
+               <a href="#">udfordringer</a>
+               <a href="#">kontak</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
