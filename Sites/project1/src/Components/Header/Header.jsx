4c20a2e2f887d5f8faa812b8bd7288e37e405637
SlankehWeb <101549379+SlankehWeb@users.noreply.github.com>
2023-01-10 11:30:29 +0100

    yup

--- Sites/project1/src/Components/Header/Header.jsx
+++ Sites/project1/src/Components/Header/Header.jsx
@@ -2,14 +2,30 @@ const Header = () => {
     return (
         <header>
             <div class="banner">
+            <img src="./ColorWheel.png" alt="colorwheel" class="banner-img"/>
+            <div class="banner-text">
+                <h1>THE GLOBAL GOALS</h1>
+                <h2>For sustainable Development</h2>
+            </div>
 
             </div>
-            <nav>
-               <a class="#" href="#">verdensmålene</a>
+            <div class="nav">
+  <input type="checkbox" id="nav-check"/>
+  <div class="nav-btn">
+    <label for="nav-check">
+      <span></span>
+      <span></span>
+      <span></span>
+    </label>
+  </div>
+  
+  <div class="nav-links">
+  <a class="#" href="#">verdensmålene</a>
                <a href="#">delmålene</a>
                <a href="#">udfordringer</a>
                <a href="#">kontak</a>
-            </nav>
+  </div>
+</div>
         </header>
     )
 }
